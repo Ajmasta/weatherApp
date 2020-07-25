8279bd0763530c861db071232209331c53bf777f
ajmastaflex <adrien.moevus@gmail.com>
2020-07-25 00:03:01 -0400

    Updates

---
+++ precache-manifest.cfd2fb2c4823cd2d8b2dc78be464747c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a8565101b2c3d0e16698d924418e27",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "126c275f0ad7b4f56275",
+    "url": "/weatherApp/static/css/main.511d3fbd.chunk.css"
+  },
+  {
+    "revision": "0d8fa80d0671a3e1f2d6",
+    "url": "/weatherApp/static/js/2.02cc1fad.chunk.js"
+  },
+  {
+    "revision": "594dcf93556a3290f6c2b1e34e642b9f",
+    "url": "/weatherApp/static/js/2.02cc1fad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126c275f0ad7b4f56275",
+    "url": "/weatherApp/static/js/main.38b4a607.chunk.js"
+  },
+  {
+    "revision": "242fe024e7577e43bea0",
+    "url": "/weatherApp/static/js/runtime-main.c734d1b3.js"
+  }
+]);
